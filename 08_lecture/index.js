d59450c972d50dce17483e1cdb27a31de90dfba6
janak-07 <janakdiyora8@gmail.com>
2024-10-16 09:16:59 +0530

    first commit

---
+++ 08_lecture/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path")
+const app = express();
+const PORT = 8000;
+
+// Middleware
+
+// app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, "public")))
+
+// app.get("/", (req, res) => {
+// //   res.end("Namaste Node");
+//      res.sendFile(path.join(__dirname, "public", "index.html"))
+// });
+
+app.listen(PORT, () => {
+  console.log("Server Started");
+});
+
